25c90e9203e43e7a9c67e3de33d2a6436b994f57
Hieu <dqhieu01@gmail.com>
2020-09-24 02:19:42 +0700

    added functions

--- src/main/java/DictionaryController.java
+++ src/main/java/DictionaryController.java
@@ -1,9 +1,20 @@
 import com.jfoenix.controls.JFXButton;
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.io.IOException;
 
 public class DictionaryController {
   @FXML private BorderPane mainContainer;
@@ -26,28 +37,56 @@ public class DictionaryController {
 
   @FXML
   private void setLeftMenuSizeOnMouseEnter() {
-    leftMenuVBox.setPrefWidth(250.0);
     menuButtonLookUp.setMinWidth(Region.USE_COMPUTED_SIZE);
     menuButtonHistory.setMinWidth(Region.USE_COMPUTED_SIZE);
     menuButtonFavorites.setMinWidth(Region.USE_COMPUTED_SIZE);
     menuButtonParaTrans.setMinWidth(Region.USE_COMPUTED_SIZE);
     menuButtonDictMan.setMinWidth(Region.USE_COMPUTED_SIZE);
     menuButtonSettings.setMinWidth(Region.USE_COMPUTED_SIZE);
+    Timeline timeline =
+        new Timeline(
+            new KeyFrame(
+                Duration.millis(200),
+                new KeyValue(leftMenuVBox.prefWidthProperty(), 250, Interpolator.EASE_BOTH)));
+
+    timeline.play();
   }
 
   @FXML
   private void setLeftMenuSizeOnMouseLeave() {
-    leftMenuVBox.setPrefWidth(50.0);
     menuButtonLookUp.setMinWidth(0);
     menuButtonHistory.setMinWidth(0);
     menuButtonFavorites.setMinWidth(0);
     menuButtonParaTrans.setMinWidth(0);
     menuButtonDictMan.setMinWidth(0);
     menuButtonSettings.setMinWidth(0);
+    Timeline timeline =
+        new Timeline(
+            new KeyFrame(
+                Duration.millis(200),
+                new KeyValue(leftMenuVBox.prefWidthProperty(), 50, Interpolator.EASE_BOTH)));
+
+    timeline.play();
   }
 
   @FXML
-  private void initialize() {
-
+  private void loadLookUpContent() throws IOException {
+    Parent scene = FXMLLoader.load(getClass().getResource("/fxml/LookupContent.fxml"));
+    mainContainer.setCenter(scene);
   }
+
+  @FXML
+  private void closeApplication() {
+    Platform.exit();
+  }
+
+  @FXML private void minimizeApplication() {
+    ((Stage)mainContainer.getScene().getWindow()).setIconified(true);
+  }
+
+  @FXML private void restoreOrMaximizeApplication() {
+    ((Stage)mainContainer.getScene().getWindow()).setMaximized(true);
+  }
+  @FXML
+  private void initialize() {}
 }
